f9a778f3dc9e9635d520789012d7d803890789d4
Abebe Gedefaw Tegegne <99889502+zalmat123@users.noreply.github.com>
2022-07-04 12:23:23 +0300

    python code commit

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" creates a square with size"""
+
+
+class Square:
+    """ Square Class
+        Attributes:
+        size (int): size of square
+    """
+    __size = 0
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        A = self.__size * self.__size
+        return (A)
